b34d47d7094c5c209d391178e2edc2141564f6ef
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-12 13:18:17 +0600

    opencv

--- Opencv/01_Image.py
+++ Opencv/01_Image.py
@@ -1,1 +1,12 @@
 import cv2
+
+img = cv2.imread('assets\pic.jpg',1)
+
+# Image Mode 
+#-1,cv2.IMREAD_COLOR
+# 0, cv2.IMREAD_GRAYSCALE
+# 1, cv2.IMREAD_UNCHANGED
+
+cv2.imshow('pic',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
