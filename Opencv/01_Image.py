011c6e7469444340b345d1db9013e20c9ddce044
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-12 13:23:44 +0600

    opencv

--- Opencv/01_Image.py
+++ Opencv/01_Image.py
@@ -7,6 +7,12 @@ img = cv2.imread('assets\pic.jpg',1)
 # 0, cv2.IMREAD_GRAYSCALE
 # 1, cv2.IMREAD_UNCHANGED
 
+img = cv2.resize(img,(500,500))
+
+img = cv2.rotate(img, cv2.ROTATE_180)
+
+cv2.imwrite('new_img.jpg',img)
+
 cv2.imshow('pic',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
